a269b48bc20cf8f4b6f88578af7ecdad7415fff0
Chris <christiane.okamoto@gmail.com>
2017-01-31 21:36:59 -0200

    configura devise

---
+++ README.md
@@ -0,0 +1,1 @@
+To-Do list in Ruby on Rails
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  devise_scope :user do
+    root to: 'devise/sessions#new'
+  end
+
+  resources :to_dos
+  get 'to_dos/:id/finish', to: 'to_dos#finish', as: :finish_to_do
+end
